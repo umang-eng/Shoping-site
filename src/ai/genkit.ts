24ecc01f177c444f50681cf4f6ed3a93aee5bea1
umang bhut <umangbhut10@gmail.com>
2025-09-21 11:49:35 +0000

    how to remove dev tool from left bottom

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,11 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+// import {nextPlugin} from '@genkit-ai/next';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI(),
+    // nextPlugin()
+  ],
   model: 'googleai/gemini-2.5-flash',
 });
